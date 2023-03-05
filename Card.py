d73eedcd83cd5c14ef587148d7b30fb6713d7bdb
Joshua Bailley <30305294+joshuab112@users.noreply.github.com>
2023-03-04 21:08:51 -0600

    Changed comment on "values" dictionary

--- Card.py
+++ Card.py
@@ -9,11 +9,7 @@ class Card(object):
 
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"]
 
-    '''
-    Dictionary that maps the ranks to their respective numerical values.
-    It should be noted that value for the Ace card is omitted because that
-    value will be chosen at the player's discretion or automatically for the dealer
-    '''
+    # Dictionary that maps the ranks to their respective numerical values.
     values = {"2": 2, "3" : 3, "4" :4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
               "Jack": 10, "King": 10, "Queen": 10, "Ace": 11}
     
